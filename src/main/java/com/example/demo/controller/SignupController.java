217ee4e915c02270abecbc0a34677fc20da5193e
minako.suzuki <sminako32@gmail.com>
2022-11-12 15:31:09 +0900

    エラーメッセージの編集

--- src/main/java/com/example/demo/controller/SignupController.java
+++ src/main/java/com/example/demo/controller/SignupController.java
@@ -1,17 +1,24 @@
 package com.example.demo.controller;
 
+import java.util.Locale;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.application.service.UserApplicationService;
+import com.example.demo.form.SignupForm;
+
+import lombok.extern.slf4j.Slf4j;
 @Controller
 @RequestMapping("/user")
+@Slf4j
 public class SignupController {
 
     @Autowired
@@ -19,9 +26,10 @@ public class SignupController {
 
     /**ユーザー登録画面を表示*/
     @GetMapping("/signup")
-    public String getSignup(Model model) {
+    public String getSignup(Model model, Locale locale,
+            @ModelAttribute SignupForm form) {
         //性別を取得
-        Map<String, Integer> genderMap = userApplicationService.getGenderMap();
+        Map<String, Integer> genderMap = userApplicationService.getGenderMap(locale);
         model.addAttribute("genderMap",genderMap);
 
         //ユーザー登録画面に遷移
@@ -31,7 +39,17 @@ public class SignupController {
 
     /**ユーザー登録処理*/
     @PostMapping("/signup")
-    public String postSignup() {
+    public String postSignup(Model model, Locale locale,
+            @ModelAttribute SignupForm form,
+            BindingResult bindingResult) {
+
+        //入力チェック結果
+        if(bindingResult.hasErrors()) {
+            //NG:ユーザー登録画面に戻ります
+            return getSignup(model,locale,form);
+        }
+
+        log.info(form.toString());
         //ログイン画面にリダイレクト
         return "redirect:/login";
 
